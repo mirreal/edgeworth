8617e804aa5fc4af1a88521ff23d3cebeccef38e
mirreal <zgkm181142521@gmail.com>
2018-03-01 23:01:19 +0800

    init package config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        commonjs: true,
+        es6: true
+    },
+    globals: {
+        atom: true
+    },
+    extends: [
+        'link-base'
+    ],
+    rules: {
+        'no-console': 0
+    }
+}
